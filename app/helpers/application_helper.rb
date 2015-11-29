7cab0220ac621579a0c98168630dfcad8ce7888c
Dennis B <bluz71@users.noreply.github.com>
2015-11-29 15:36:39 +1100

    Only admins can see the create and delete project links.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,11 @@ module ApplicationHelper
       end
     end
   end
+
+  def admins_only(&block)
+    # Use try here instead of current_user.admin? since if the client is not
+    # logged in the current_user will return nil thereby raising NoMethodError,
+    # which is not desired.
+    block.call if current_user.try(:admin?)
+  end
 end
